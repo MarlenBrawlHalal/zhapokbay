1305e959cabfb150748ba6d16fe82309ec608f5b
MarlenBrawlHalal <104462234+MarlenBrawlHalal@users.noreply.github.com>
2023-02-02 03:42:19 +0600

    123

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v0d$n+^^-vxvmn%1rwbl5)tt#^it5ll+p$&!a@cl_(@1-6_*=8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Marlen0007.pythonanywhere.com']
 
 
 # Application definition
